0c4d7ee1cff2cadf1b4a2b4d2cb0df58f3977400
wtping <15908282659@163.com>
2021-12-07 15:09:53 +0800

    update smart lock on c11

--- test/mutex/AutoLock.cpp
+++ test/mutex/AutoLock.cpp
@@ -2,14 +2,20 @@
 #include <thread>
 #include <mutex>
 
-class AutoLock
+class NoCopyAble
+{
+public:
+    NoCopyAble() = default;
+    NoCopyAble(NoCopyAble&) = delete;
+    NoCopyAble(NoCopyAble&&) = delete;
+    void operator=(NoCopyAble&) = delete;
+    void operator=(NoCopyAble&&) = delete;
+};
+
+class AutoLock : public NoCopyAble
 {
 public:
     AutoLock() = delete;
-    AutoLock(AutoLock&) = delete;
-    AutoLock(AutoLock&&) = delete;
-    // void operator=(AutoLock) = delete;
-    // AutoLock& operator=(AutoLock) = delete;
 
     AutoLock(std::mutex& lock)
         :_lock(lock)
@@ -22,6 +28,41 @@ public:
     }
 private:
     std::mutex& _lock;
+};
+
+class SmartLock : public NoCopyAble
+{
+public:
+    SmartLock() = delete;
+
+    SmartLock(std::mutex& lock)
+        : _lock(lock)
+        , _isLock(false)
+    {
+       this->lock();
+    }
+    ~SmartLock()
+    {
+        this->unlock();
+    }
+    void lock()
+    {
+        if(!_isLock) {
+            _lock.lock();
+            _isLock = true;
+        }
+    }
+    void unlock()
+    {
+        if(_isLock) {
+            _lock.unlock();
+            _isLock = false;
+        }
+    }
+
+private:
+    std::mutex& _lock;
+    bool _isLock;
 };
 
 class Test
@@ -41,20 +82,25 @@ private:
 
 int main(int argc, char const *argv[])
 {
+    std::cout << "1" << std::endl;
     std::mutex mutex;
     AutoLock lock(mutex);
-    // AutoLock lock2(lock);
-    // AutoLock lock3 = lock;
-    // AutoLock lock4(std::move(lock));
+    // AutoLock lock2(lock);  //Error
+    // AutoLock lock3 = lock;  //Error  //Error
+    // AutoLock lock4(std::move(lock));  //Error
 
-    AutoLock lock5 = lock;
+    // AutoLock lock5 = lock;
+    std::cout << "2" << std::endl;
     AutoLock& lock6 = lock;
+    std::cout << "3" << std::endl;
     AutoLock& lock7 = lock6;
-
-    // std::thread thread[5];
-    // for(int i=0; i<5; i++)
-    // {
-    //     thread[i].
-    // }
+    std::cout << "4" << std::endl;
+    std::mutex smutex;
+    SmartLock locks1(smutex);
+    std::cout << "5" << std::endl;
+    locks1.unlock();
+    std::cout << "6" << std::endl;
+    // SmartLock locks2(locks1);  //Error
+    // SmartLock locks3 = locks1;  //Error
     return 0;
 }
