0e55761731206ccbfefb9d87e59603cbadf9cfd2
wtping <15908282659@163.com>
2021-12-09 20:22:51 +0800

    fix max vla test

--- test/vla-array/vla.c
+++ test/vla-array/vla.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+int number = 1024*2;
 
-void test(int n)
+void test(size_t n)
 {
-    int size = 1024*2;
-    char c = 0;
-    char tbuf[size];
-    printf("size_ad :%p\n", &size);
-    printf("c  addr :%p\n", &c);
-    printf("tbuf[n] :%p\n", &tbuf[size-1]);
-    printf("tbuf[16]:%p\n", &tbuf[16]);
-    printf("tbuf[10]:%p\n", &tbuf[10]);
-    printf("tbuf[0] :%p\n", &tbuf[0]);
+    size_t size = 1024*2*n;
+    char buf[size];
+    printf("n :%llu\n", n);
+    printf("size :%llu\n", size);
+    printf("buf[0] :%p\n", &buf[0]);
+    printf("buf size :%llu\n", sizeof(buf));
+    printf("---------------------------\n");
+}
+
+void test_TheFirstThingToTest()
+{
+    for(int i = 0; i < 10000; i++)
+    {
+        test(i+1);
+    }
+
 }
 
 int main(int argc, char const *argv[])
 {
-    test(1);
+    test_TheFirstThingToTest();
     return 0;
 }
