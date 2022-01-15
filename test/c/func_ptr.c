bc3650930d30f83c3823fd129ea6e45f28c8c4ee
wtping <15908282659@163.com>
2022-01-15 16:48:05 +0800

    add func_ptr

---
+++ test/c/func_ptr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef void (*func)(int);
+
+typedef void (*signal1)(int, func);
+
+typedef void (*signal2(int, func))(int);    //????
+
+void handle_func(int num)
+{
+
+}
+
+void signal1_func(int num, func f)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    func f = handle_func;
+    signal1 l = signal1_func;
+    signal2 s2 = &l;
+    signal1* sp = &l;
+
+    f(1);
+    l(2, f);
+    (*sp)(3, f);
+
+    return 0;
+}
